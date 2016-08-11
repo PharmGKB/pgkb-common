1df7d2aac3105fabae69634ff3d08e49977ae3db
Ryan Whaley <whaleyr@pharmgkb.org>
2016-08-11 14:12:57 -0700

    Add more test cases for HaplotypeNameComparator

--- src/test/java/org/pharmgkb/common/comparator/HaplotypeNameComparatorTest.java
+++ src/test/java/org/pharmgkb/common/comparator/HaplotypeNameComparatorTest.java
@@ -6,6 +6,11 @@
  */
 package org.pharmgkb.common.comparator;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -33,6 +38,11 @@ public class HaplotypeNameComparatorTest {
     assertEquals(1, HaplotypeNameComparator.getComparator().compare("*13", "*3"));
     assertEquals(1, HaplotypeNameComparator.getComparator().compare("H3", "H2"));
 
+    assertEquals(-1, HaplotypeNameComparator.getComparator().compare("Any", "*1"));
+    assertEquals(1, HaplotypeNameComparator.getComparator().compare("*1", "Any"));
+    assertEquals(-1, HaplotypeNameComparator.getComparator().compare("*1", "Other"));
+    assertEquals(1, HaplotypeNameComparator.getComparator().compare("Unknown", "*1"));
+
     assertEquals(0, HaplotypeNameComparator.getComparator().compare("H2 EM", "H2 EM"));
     assertTrue(HaplotypeNameComparator.getComparator().compare("H2 EM", "H2 PM") < 0);
     assertTrue(HaplotypeNameComparator.getComparator().compare("H2 PM", "H2 EM") > 0);
@@ -40,5 +50,23 @@ public class HaplotypeNameComparatorTest {
     assertTrue(HaplotypeNameComparator.getComparator().compare("*1", "H2") < 0);
     assertTrue(HaplotypeNameComparator.getComparator().compare("*1", "TPMT") < 0);
     assertTrue(HaplotypeNameComparator.getComparator().compare("TPMT", "BRCA") > 0);
+  }
+  
+  @Test
+  public void testCollectionSorting() {
+    List<String> input = new ArrayList<>();
+    input.add("*3");
+    input.add("Other");
+    input.add("Any");
+    input.add("*1");
+    
+    Set<String> orderedSet = new TreeSet<>(HaplotypeNameComparator.getComparator());
+    orderedSet.addAll(input);
+    
+    Iterator<String> it = orderedSet.iterator();
+    assertEquals("Any", it.next());
+    assertEquals("*1", it.next());
+    assertEquals("*3", it.next());
+    assertEquals("Other", it.next());
   }
 }
