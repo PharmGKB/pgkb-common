dce6dfcfb5faced0ed28e9e8ba1630132d650374
Mark Woon <markwoon@gmail.com>
2017-07-12 12:28:27 -0700

    Add isValidUrl() and isReachableUrl().

--- src/main/java/org/pharmgkb/common/util/UrlUtils.java
+++ src/main/java/org/pharmgkb/common/util/UrlUtils.java
@@ -1,8 +1,14 @@
 package org.pharmgkb.common.util;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Sets;
 
 
@@ -13,6 +19,10 @@ import com.google.common.collect.Sets;
  */
 public class UrlUtils {
   private static final Set<String> sf_webSchemes = Sets.newHashSet("http", "https");
+  private static final Set<String> sf_urlSchemes = Sets.newHashSet("http", "https", "ftp");
+  private static final Pattern sf_172Pattern = Pattern.compile("^172\\.(\\d+)\\.\\d+\\.\\d+$");
+  /** Maximum amount to wait while trying to connect in ms. */
+  private static final int sf_timeout = 1000;
 
 
 
@@ -24,21 +34,119 @@ public class UrlUtils {
 
 
   /**
-   * Checks if {@code urlString} is a valid {@code http} or {@code https} URL.
+   * Checks if {@code urlString} is a syntactically valid {@code http} or {@code https} URL.
+   * <ul>
+   *   <li>Does NOT allow localhost.</li>
+   *   <li>Does NOT allow private IPs.</li>
+   * </ul>
    */
-  public static boolean isValidWebUrl(String urlString) {
+  public static boolean isValidWebUrl(@Nonnull String urlString) {
+    return isValidUrl(urlString, true, false, false, false);
+  }
+
+
+  /**
+   * Checks if {@code urlString} is a syntactically valid {@code http}, {@code https} or {@code ftp} URL.
+   * <ul>
+   *   <li>Does NOT allow localhost.</li>
+   *   <li>Does NOT allow private IPs.</li>
+   * </ul>
+   */
+  public static boolean isValidUrl(@Nonnull String urlString) {
+    return isValidUrl(urlString, false, false, false, false);
+  }
+
+
+  /**
+   * Checks if {@code urlString} is a valid {@code http}, {@code https} or {@code ftp} URL.
+   *
+   * @param noFtp true if ftp protocol is not allowed
+   * @param allowLocalhost true if localhost should be allowed
+   * @param allowPrivateIp true if private IP addresses should be allowed
+   * @param verify true to check if URL actually points to something (see caveats for {@link #isReachableUrl(URL)})
+   */
+  public static boolean isValidUrl(@Nonnull String urlString, boolean noFtp, boolean allowLocalhost,
+      boolean allowPrivateIp, boolean verify) {
 
     try {
-      URI uri = new URL(urlString).toURI();
-      if (!sf_webSchemes.contains(uri.getScheme())) {
+      URL url = new URL(urlString);
+      URI uri = url.toURI();
+      Set<String> schemes = noFtp ? sf_webSchemes : sf_urlSchemes;
+      if (!schemes.contains(uri.getScheme())) {
         return false;
       }
       if (uri.getHost() == null) {
         return false;
       }
+      if (uri.getRawUserInfo() != null && Splitter.on(":").splitToList(uri.getRawUserInfo()).size() > 2) {
+        return false;
+      }
+
+      if (!allowLocalhost) {
+        if (uri.getHost().equals("localhost") || uri.getHost().startsWith("localhost.")) {
+          return false;
+        }
+      }
+      if (!allowPrivateIp) {
+        // ipv6
+        if (uri.getHost().startsWith("[fd") ||
+            // ipv4
+            uri.getHost().equals("127.0.0.1") ||
+            uri.getHost().startsWith("10.") ||
+            uri.getHost().startsWith("192.168.")) {
+          return false;
+        }
+        Matcher m = sf_172Pattern.matcher(uri.getHost());
+        if (m.matches() && Integer.parseInt(m.group(1)) >= 16 && Integer.parseInt(m.group(1)) <= 32) {
+          return false;
+        }
+      }
+
+      if (verify) {
+        if (!isReachableUrl(url)) {
+          return false;
+        }
+      }
       return true;
 
     } catch (Exception e) {
+      return false;
+    }
+  }
+
+
+  /**
+   * For HTTP URLs, this will perform a HEAD request and checks for a non-error response.
+   * For FTP URLs, this will only verify that we can connect to the server, not whether the resource is actually
+   * available.
+   */
+  public static boolean isReachableUrl(@Nonnull URL url) {
+    try {
+      if (sf_webSchemes.contains(url.getProtocol())) {
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setConnectTimeout(sf_timeout);
+        connection.setReadTimeout(sf_timeout);
+        connection.setRequestMethod("HEAD");
+        // use no encoding to avoid gzip, HEAD has no body anyway (see https://issuetracker.google.com/issues/36939140)
+        connection.setRequestProperty("Accept-Encoding", "");
+        int responseCode = connection.getResponseCode();
+        return (200 <= responseCode && responseCode <= 399);
+
+      } else {
+        URLConnection connection = url.openConnection();
+        connection.setConnectTimeout(sf_timeout);
+        connection.setReadTimeout(sf_timeout);
+        // TODO(markwoon): this only checks to see if we can connect to the server
+        connection.connect();
+        // TODO(markwoon): we're not actualy verifying if the file is available, or if it is even a file
+        /* unreliable, fails for ftp://anonymous:foo%40bar.com@ftp.swfwmd.state.fl.us/pub/README.txt
+        try (InputStream in = connection.getInputStream()) {
+        }
+        */
+        return true;
+      }
+
+    } catch (Exception ex) {
       return false;
     }
   }
--- src/test/java/org/pharmgkb/common/util/UrlUtilsTest.java
+++ src/test/java/org/pharmgkb/common/util/UrlUtilsTest.java
@@ -1,5 +1,6 @@
 package org.pharmgkb.common.util;
 
+import java.net.URL;
 import net.trajano.commons.testing.UtilityClassTestUtil;
 import org.junit.Test;
 
@@ -21,7 +22,7 @@ public class UrlUtilsTest {
 
 
   @Test
-  public void testUrls() {
+  public void testWebUrls() {
     assertTrue(UrlUtils.isValidWebUrl("http://www.pharmgkb.org"));
     assertTrue(UrlUtils.isValidWebUrl("http://www.pharmgkb.org/bar.txt"));
     assertTrue(UrlUtils.isValidWebUrl("http://www.pharmgkb.org:8080"));
@@ -39,16 +40,110 @@ public class UrlUtilsTest {
     assertFalse(UrlUtils.isValidWebUrl("ftp://hi:there@www.pharmgkb.org:8080"));
     assertFalse(UrlUtils.isValidWebUrl("ftp://hi:there@www.pharmgkb.org:8080/bar.txt"));
 
-    assertTrue(UrlUtils.isValidWebUrl("http://localhost"));
-    assertTrue(UrlUtils.isValidWebUrl("http://127.0.0.1"));
-    assertTrue(UrlUtils.isValidWebUrl("http://10.0.0.1:8080"));
-
+    assertTrue(UrlUtils.isValidWebUrl("http://171.67.192.16"));
+    assertTrue(UrlUtils.isValidWebUrl("http://171.67.192.16:8080"));
+    assertTrue(UrlUtils.isValidWebUrl("http://172.168.15.1"));
+    assertTrue(UrlUtils.isValidWebUrl("http://172.168.33.1"));
     // ipv6
     assertTrue(UrlUtils.isValidWebUrl("http://[2001:db8:a0b:12f0::1]/index.html"));
     assertTrue(UrlUtils.isValidWebUrl("http://[2001:db8:a0b:12f0::1]:80/index.html"));
 
+    // localhost
+    assertFalse(UrlUtils.isValidWebUrl("http://localhost"));
+    // private ip
+    assertFalse(UrlUtils.isValidWebUrl("http://127.0.0.1"));
+    assertFalse(UrlUtils.isValidWebUrl("http://10.0.0.1:8080"));
+    assertFalse(UrlUtils.isValidWebUrl("http://172.16.1.1"));
+    assertFalse(UrlUtils.isValidWebUrl("http://172.22.1.1"));
+    assertFalse(UrlUtils.isValidWebUrl("http://172.32.1.1"));
+    assertFalse(UrlUtils.isValidWebUrl("http://[fd01:db8:a0b:12f0::1]:80/index.html"));
+
     assertFalse(UrlUtils.isValidWebUrl("http://"));
     assertFalse(UrlUtils.isValidWebUrl("http://."));
+    assertFalse(UrlUtils.isValidWebUrl("http://.com"));
     assertFalse(UrlUtils.isValidWebUrl("https:// "));
+    assertFalse(UrlUtils.isValidWebUrl("ftp://"));
+    assertFalse(UrlUtils.isValidWebUrl("ftp://::::@example.com"));
+    assertFalse(UrlUtils.isValidWebUrl("mailto:foo@bar.com"));
+  }
+
+
+  @Test
+  public void testUrls() {
+    assertTrue(UrlUtils.isValidUrl("http://www.pharmgkb.org"));
+    assertTrue(UrlUtils.isValidUrl("http://www.pharmgkb.org/bar.txt"));
+    assertTrue(UrlUtils.isValidUrl("http://www.pharmgkb.org:8080"));
+    assertTrue(UrlUtils.isValidUrl("http://www.pharmgkb.org:8080/bar.txt"));
+    assertTrue(UrlUtils.isValidUrl("http://hi:there@www.pharmgkb.org/"));
+
+    assertTrue(UrlUtils.isValidUrl("https://www.pharmgkb.org"));
+    assertTrue(UrlUtils.isValidUrl("https://www.pharmgkb.org/bar.txt"));
+    assertTrue(UrlUtils.isValidUrl("https://www.pharmgkb.org:8080"));
+    assertTrue(UrlUtils.isValidUrl("https://www.pharmgkb.org:8080/bar.txt"));
+    assertTrue(UrlUtils.isValidUrl("https://hi:there@www.pharmgkb.org/"));
+
+    assertTrue(UrlUtils.isValidUrl("ftp://www.pharmgkb.org"));
+    assertTrue(UrlUtils.isValidUrl("ftp://www.pharmgkb.org/bar.txt"));
+    assertTrue(UrlUtils.isValidUrl("ftp://hi:there@www.pharmgkb.org:8080"));
+    assertTrue(UrlUtils.isValidUrl("ftp://hi:there@www.pharmgkb.org:8080/bar.txt"));
+
+    assertTrue(UrlUtils.isValidUrl("http://171.67.192.16"));
+    assertTrue(UrlUtils.isValidUrl("http://171.67.192.16:8080"));
+    assertTrue(UrlUtils.isValidUrl("http://172.168.15.1"));
+    assertTrue(UrlUtils.isValidUrl("http://172.168.33.1"));
+    // ipv6
+    assertTrue(UrlUtils.isValidUrl("http://[2001:db8:a0b:12f0::1]/index.html"));
+    assertTrue(UrlUtils.isValidUrl("http://[2001:db8:a0b:12f0::1]:80/index.html"));
+
+    // localhost
+    assertFalse(UrlUtils.isValidUrl("http://localhost"));
+    // private ip
+    assertFalse(UrlUtils.isValidUrl("http://127.0.0.1"));
+    assertFalse(UrlUtils.isValidUrl("http://10.0.0.1:8080"));
+    assertFalse(UrlUtils.isValidUrl("http://172.16.1.1"));
+    assertFalse(UrlUtils.isValidUrl("http://172.22.1.1"));
+    assertFalse(UrlUtils.isValidUrl("http://172.32.1.1"));
+    assertFalse(UrlUtils.isValidUrl("http://[fd01:db8:a0b:12f0::1]:80/index.html"));
+
+    assertFalse(UrlUtils.isValidUrl("http://"));
+    assertFalse(UrlUtils.isValidUrl("http://."));
+    assertFalse(UrlUtils.isValidUrl("http://.com"));
+    assertFalse(UrlUtils.isValidUrl("https:// "));
+    assertFalse(UrlUtils.isValidUrl("ftp://"));
+    assertFalse(UrlUtils.isValidUrl("ftp://::::@example.com"));
+    assertFalse(UrlUtils.isValidUrl("mailto:foo@bar.com"));
+  }
+
+
+  @Test
+  public void testLocalAndPrivateUrls() {
+
+    // localhost
+    assertTrue(UrlUtils.isValidUrl("http://localhost", true, true, true, false));
+    // private ip
+    assertTrue(UrlUtils.isValidUrl("http://127.0.0.1", true, true, true, false));
+    assertTrue(UrlUtils.isValidUrl("http://10.0.0.1:8080", true, true, true, false));
+    assertTrue(UrlUtils.isValidUrl("http://172.16.1.1", true, true, true, false));
+    assertTrue(UrlUtils.isValidUrl("http://172.22.1.1", true, true, true, false));
+    assertTrue(UrlUtils.isValidUrl("http://172.32.1.1", true, true, true, false));
+    assertTrue(UrlUtils.isValidUrl("http://[fd01:db8:a0b:12f0::1]:80/index.html", true, true, true, false));
+  }
+
+
+  @Test
+  public void testIsReachableUrl() throws Exception {
+
+    assertTrue(UrlUtils.isReachableUrl(new URL("http://www.pharmgkb.org")));
+    assertTrue(UrlUtils.isReachableUrl(new URL("https://www.pharmgkb.org")));
+    assertTrue(UrlUtils.isReachableUrl(new URL("ftp://mirrors.sonic.net/cygwin/")));
+    assertTrue(UrlUtils.isReachableUrl(new URL("ftp://anonymous:foo%40bar.com@ftp.swfwmd.state.fl.us/pub/README.txt")));
+  }
+
+
+  @Test
+  public void testVerify() {
+
+    assertTrue(UrlUtils.isValidUrl("http://www.pharmgkb.org", true, false, false, true));
+    assertFalse(UrlUtils.isValidUrl("http://www.pharmgkboo.org", true, false, false, true));
   }
 }
