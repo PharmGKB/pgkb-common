d24f808ebdd1a4aabbee8df40e4039a010e4e242
Mark Woon <markwoon@gmail.com>
2018-02-27 15:49:56 -0800

    Add method to close Closeables quietly.

---
+++ src/main/java/org/pharmgkb/common/util/IoUtils.java
@@ -0,0 +1,32 @@
+package org.pharmgkb.common.util;
+
+import java.io.Closeable;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Utility methods for working with I/O.
+ *
+ * @author Mark Woon
+ */
+public class IoUtils {
+  private static final Logger sf_logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+  /**
+   * Private constructor to prevent instantiation of utility class.
+   */
+  private IoUtils() {
+  }
+
+
+  public static void closeQuietly(Closeable closeable) {
+    try {
+      closeable.close();
+    } catch (Exception ex) {
+      sf_logger.warn("Error closing {}", closeable.getClass(), ex);
+    }
+  }
+}
