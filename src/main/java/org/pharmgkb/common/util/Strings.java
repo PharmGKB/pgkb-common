465785b9c597330ef6124215b0f0dc2b212796be
Mark Woon <markwoon@gmail.com>
2021-03-31 11:41:53 -0700

    feat: add Strings utility class

---
+++ src/main/java/org/pharmgkb/common/util/Strings.java
@@ -0,0 +1,41 @@
+package org.pharmgkb.common.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+
+/**
+ * Utility functions for {@link String}s.
+ *
+ * @author Mark Woon
+ */
+public class Strings {
+
+  /**
+   * Private constructor to prevent instantiation of utility class.
+   */
+  private Strings() {
+  }
+
+
+  /**
+   * Wraps {@link StringUtils#stripToEmpty(String)} (String)} to also strip out {@code &nbsp;} (\u00A0).
+   */
+  public static String stripToEmpty(@Nullable String str) {
+    if (str == null) {
+      return "";
+    }
+    return StringUtils.stripToEmpty(str.replaceAll("\u00A0", ""));
+  }
+
+
+  /**
+   * Wraps {@link StringUtils#stripToNull(String)} to also strip out {@code &nbsp;} (\u00A0).
+   */
+  public static @Nullable String stripToNull(@Nullable String str) {
+    if (str == null) {
+      return null;
+    }
+    return StringUtils.stripToNull(str.replaceAll("\u00A0", ""));
+  }
+}
